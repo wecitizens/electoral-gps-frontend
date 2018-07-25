0f0a252e4ad31c8f666dece1aedd7ae453314fe4
olivier5741 <olivier5741@gmail.com>
2018-07-25 20:41:27 +0200

    json on amazon not on https

--- store/questions/services.js
+++ store/questions/services.js
@@ -2,6 +2,6 @@ import API from '../_common/services/api'
 
 export const questionsService = {
   getQuestions (data) {
-    return API.get('http://wecitizens.s3-website.eu-central-1.amazonaws.com/questions.json')
+    return API.get('https://olivier5741.github.io/wecitizens_api_doc/api/work_in_progress/questions-without-i18n.json')
   }
 }
