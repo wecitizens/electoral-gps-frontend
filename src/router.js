9aaef5cfd313b8d008df57c477731252527638b9
Daniel Sum <daniel@lacerisenumerique.be>
2018-09-04 20:46:17 +0200

    Add Survey

--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Results from './views/Results.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Results from './views/Results.vue';
+import Survey from './views/Survey.vue';
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/survey',
+      name: 'survey',
+      component: Survey
     },
     {
       path: '/results',
---
+++ src/views/Survey.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="results">
+    <h1>Results</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'results',
+  components: {
+    HelloWorld
+  }
+}
+</script>
