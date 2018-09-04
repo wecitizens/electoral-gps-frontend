c92cba4fd32570593d789b2328efa54c3ab17ed4
Daniel Sum <daniel@lacerisenumerique.be>
2018-09-04 21:23:44 +0200

    - remove helloworld - add simplified store (W.I.P)

--- src/store.js
+++ src/store.js
@@ -1,16 +1,32 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
+  getters: {
+    questions: state => state
+  },
   state: {
-
+    questions : []
   },
   mutations: {
-
+    setAnswer(){
+      console.log('todo');
+    }
   },
   actions: {
-
+    getQuestions (data) {
+      axios.get('https://wecitizens.github.io/wecitizens_api_doc/api/gps/survey/2018_be_municipal_wallonia_rural.json', data).then((res) => {
+        console.log(res);
+      });
+    },
+    setQuestionAgreement ({commit, state}, data) {
+      console.log('TODO');
+    },
+    setQuestionImportance ({commit}, data) {
+      console.log('TODO');
+    }
   }
 })
--- src/views/Survey.vue
+++ src/views/Survey.vue
@@ -1,13 +1,10 @@
 <template>
   <div class="results">
     <h1>Results</h1>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'results',
