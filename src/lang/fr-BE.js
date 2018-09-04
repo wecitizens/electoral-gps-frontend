1847c6ed4d95b4c00c215eb098870118a540610b
Daniel Sum <daniel@lacerisenumerique.be>
2018-09-04 20:20:13 +0200

    - update styling - try to install jquery for scroll to effect (didn't work :/)

---
+++ src/lang/fr-BE.js
@@ -0,0 +1,68 @@
+const config = {
+  app: {
+    title: 'GPS électoral'
+  },
+  association: {
+  	name: 'WeCitizens'
+  },
+  district: {
+    what_is_your_postcode: 'Quel est ton code postal ?',
+    help_customise_survey: 'Ceci nous aidera à personnaliser ton questionnaire'
+  },
+  importance: {
+    '1': 'Pas important',
+    '2': 'Peu important',
+    '3': 'Moyen',
+    '4': 'Important',
+    '5': 'Très important'
+  },
+  home: {
+    title: 'Trouve les candidats qui te correspondent',
+    cta: 'Je fais le test!',
+    description: 'Déjà {userCount} matchs',
+    with_help_from: 'Avec le soutient de :',
+    powered_by: 'Powered by:'
+  },
+  menu: {
+  	item: {
+      welcome: 'Accueil',
+      go_to_results: 'Sauter aux résultats',
+      share_app: 'Partager le {{app.title}}',
+      share_results: 'Partager mes résultats',
+      see_all_questions: 'Voir toutes les questions',
+      start_from_scratch: 'Recommencer à zéro',
+      about_app: 'À propos du {{app.title}}',
+      about_association: 'À propos de {{association.name}}',
+      municipality: 'Commune',
+      questions: 'Questions',
+      stats: 'Stats',
+      results: 'Résultats'
+  	}
+  },
+  results: {
+  	candatidates_sharing_my_position_the_most: 'Les candidats qui partagent le plus mes convictions'
+  },
+  stats: {
+    anonymous_question_to_improve_service: 'Ces dernières questions restent anonymes et nous permettent d’améliorer notre service',
+    less_than_35: '-35 ans',
+    between_35_and_55: '35 à 55',
+    over_55: '+ 55 ans'
+  },
+  button: {
+    lets_go: 'C\'est parti',
+    see_results: 'Voir les résultats',
+    previous_question: 'Previous Question',
+    more_info: '+ more infos'
+  },
+  title: {
+  	candidates: 'Candidats',
+  	parties: 'Listes'
+  },
+  input: {
+    place_holder: {
+      your_postcode: 'Ton code postal'
+    }
+  }
+}
+
+module.exports = config
--- src/main.js
+++ src/main.js
@@ -1,13 +1,32 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './registerServiceWorker';
+import vuexI18n from 'vuex-i18n';
 
-Vue.config.productionTip = false
+import fr from './lang/fr-BE';
+import nl from './lang/nl-BE';
+import en from './lang/en-BE';
+
+Vue.i18n.add('fr', fr);
+Vue.i18n.add('nl', nl);
+Vue.i18n.add('en', en);
+
+// set the start locale to use
+Vue.i18n.set('en');
+
+Vue.config.productionTip = false;
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue);
+Vue.use(vuexI18n.plugin, store);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
