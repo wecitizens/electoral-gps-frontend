327e2eb2ed0d17169989b4673e76ac7586850bfe
olivier5741 <olivier5741@gmail.com>
2018-09-06 14:52:45 +0200

    utils - polls generation missing base data

---
+++ utils/poll_generation/generate-polls.js
@@ -0,0 +1,34 @@
+const csv = require('csvtojson');
+const fs = require("fs");
+
+const fileName = "districts.csv";
+
+csv()
+    .fromFile(fileName)
+    .then((districts) => {
+
+        districts.forEach(district => {
+            
+            const ep = "2018_be_municipal_"; 
+            
+            let poll = {
+                key: ep + district.code + "_voter",
+                survey_key: ep + district.region + "_" + district.type,
+                segment_keys : [
+                    ep + district.code + "_politician",
+                    ep + district.code + "_party"
+                ],
+                insufficient_data: false
+            };
+
+            fs.writeFile(poll.key + ".json", JSON.stringify(poll, null, 2), (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+
+                console.log("File has been created");
+            });
+        });
+
+    });
