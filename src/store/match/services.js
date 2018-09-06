3295a3731673789bcc09a93749f8c94965253fbf
olivier5741 <olivier5741@gmail.com>
2018-09-06 23:29:58 +0200

    matching - test (in utils) fully works

--- src/store/match/services.js
+++ src/store/match/services.js
@@ -1,4 +1,6 @@
 import API from "../_helpers/api";
+import LA from 'look-alike';
+import LAUtil from '../../../node_modules/look-alike/lib/util';
 
 const matchService = {
     
@@ -8,57 +10,56 @@ const matchService = {
         // further implementation, see code in https://github.com/wecitizens/electoral-gps-frontend/issues/17
         
         // stub data
-        matchRequest  = {
+
+        matchRequest = {
             "segment_key": '2018_be_municipal_be_6850_politician',
             "answer_formats":
                 [
                     {
                         "key": "agr_5_scale_tol_3_scale_abs",
-                        "items":
-                            [
-                                {
-                                    "key": "fully_agree",
-                                    "name": "answer_format_item_fully_agree",
-                                    "weight": 100
-                                },
-                                {
-                                    "key": "rather_yes",
-                                    "name": "answer_format_rather_yes",
-                                    "weight": 75
-                                },
-                                {
-                                    "key": "no_opinion",
-                                    "name": "answer_format_item_no_opinion",
-                                    "weight": 50
-                                },
-                                {
-                                    "key": "rather_no",
-                                    "name": "answer_format_item_rather_no",
-                                    "weight": 25
-                                },
-                                {
-                                    "key": "strongly_disagree",
-                                    "name": "answer_format_item_strongly_disagree",
-                                    "weight": 0
-                                }
-                            ],
+                        "items": [
+                            {
+                                "key": "strongly_agree",
+                                "name": "answer_format.item.strongly_agree",
+                                "weight": 100
+                            },
+                            {
+                                "key": "agree",
+                                "name": "answer_format.item.agree",
+                                "weight": 75
+                            },
+                            {
+                                "key": "no_opinion",
+                                "name": "answer_format.item.no_opinion",
+                                "weight": 50
+                            },
+                            {
+                                "key": "disagree",
+                                "name": "answer_format.item.disagree",
+                                "weight": 25
+                            },
+                            {
+                                "key": "strongly_disagree",
+                                "name": "answer_format.item.strongly_disagree",
+                                "weight": 0
+                            }
+                        ],
                         "tolerance": {
-                            "absolute": true,
                             "items": [
                                 {
-                                    "key": "very_important",
-                                    "name": "answer_format_tolerance_item_very_important",
-                                    "weight": 2.5
+                                    "key": "not_important",
+                                    "name": "answer_format.tolerance.item.not_important",
+                                    "weight": 0.4
                                 },
                                 {
                                     "key": "important",
-                                    "name": "answer_format_tolerance_item_important",
+                                    "name": "answer_format.tolerance.item.important",
                                     "weight": 1
                                 },
                                 {
-                                    "key": "not_important",
-                                    "name": "answer_format_tolerance_item_not_important",
-                                    "weight": 0.4
+                                    "key": "very_important",
+                                    "name": "answer_format.tolerance.item.very_important",
+                                    "weight": 2.5
                                 }
                             ]
                         }
@@ -66,27 +67,137 @@ const matchService = {
                 ],
             answers: [
                 {
-                    question_key: "q1",
+                    question_key: "question_103",
                     answer_format: "agr_5_scale_tol_3_scale_abs",
-                    value: "rather_yes",
+                    value: "agree",
                     tolerance: "very_important"
                 },
                 {
-                    question_key: "q2",
+                    question_key: "question_104",
                     answer_format: "agr_5_scale_tol_3_scale_abs",
-                    value: "rather_no",
-                    tolerance: "not_important"
+                    value: "disagree",
+                    tolerance: "important"
                 }
             ]
         };
 
-        const data = await API.get('/api/vote/district.json', data).then((request) => {
-            return request.data.data
-        })
+        const data = [
+            {
+                "user_key": "be_politician_1",
+                "question_key": "question_103",
+                "value": "agree"
+            },
+            {
+                "user_key": "be_politician_1",
+                "question_key": "question_104",
+                "value": "strongly_disagree"
+            },
+            {
+                "user_key": "be_politician_2",
+                "question_key": "question_103",
+                "value": "agree"
+            },
+            {
+                "user_key": "be_politician_2",
+                "question_key": "question_104",
+                "value": "strongly_agree"
+            }
+        ];
+
+        const answer_formats = matchRequest.answer_formats;
+        const a = matchRequest.answers;
+
+        const subject = matchRequest.answers.reduce(
+            (accumulator, target) => ({
+                ...accumulator,
+                [target.question_key]:
+                answer_formats.find(x => x.key = target.answer_format)
+                    .items.find(x => x.key === target.value).weight
+            }),
+            {});
+
+        const weights = matchRequest.answers.reduce(
+            (accumulator, target) => ({
+                ...accumulator,
+                [target.question_key]:
+                answer_formats.find(x => x.key = target.answer_format).tolerance
+                    .items.find(x => x.key === target.tolerance).weight
+            }),
+            {});
+
+        const t = data.reduce(
+            function (acc, t) {
+
+                let u = {};
+
+                if (acc.hasOwnProperty(t.user_key))
+                    u = acc[t.user_key];
+
+                const q = a.find(aa => aa.question_key == t.question_key);
+
+                u[t.question_key] = answer_formats.find(x => x.key == q.answer_format).items.find(x => x.key ==  t.value).weight;
+                acc[t.user_key] = u;
+                return acc;
+
+            }
+            , {});
+
+
+        console.log("My choice : ");
+        console.log(subject);
+
+        console.log("My tolerance : ");
+        console.log(weights);
+
+        const answers = Object.keys(t).map(k => {return {key: k, answers: t[k]}});
+        console.log("Segment answers : ");
+        console.log(answers);
+
+       const tree = new LA(answers,
+            null,
+            function (r) {
+                return r.answers;
+            }
+        );
+
+        const results = tree.query({key: "subject", answers: subject}, {
+            k: answers.length, // number of match
+            normalize: false,
+            weights: weights
+        });
+
+// works for one format only
+        const itemWeightMax = answer_formats[0].items.map(i => i.weight).reduce((p,c) => {
+            return c > p ? c : p;
+        },0);
+
+        const distanceMax = Math.sqrt(
+            Object.values(weights).reduce((p,c) => {
+                return p + Math.pow(c*itemWeightMax,2)
+            }, 0));
+
+        console.log("Max weight : ");
+        console.log(itemWeightMax);
+
+        console.log("Max distance : ");
+        console.log(distanceMax);
+
+        const scores = results.map(r => {
+
+            const distance = LAUtil.distance(r.answers, subject, {weights: weights});
+            const match = 100 * (1 - distance / distanceMax);
+
+            return {
+                key: r.key,
+                score: match
+            }
+        });
+
+        console.log("My scores : ");
+        console.log(scores);
         
+        return scores;      
         
-        
-        // return list of users with the score
     }
 };
 
