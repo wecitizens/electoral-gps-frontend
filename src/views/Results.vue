b2497c4e83aa98257f973281b19e4417a5ae9c09
olivier5741 <olivier5741@gmail.com>
2018-09-07 08:40:36 +0200

    results - plug matching - part 1

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -3,17 +3,17 @@
         <b-card no-body>
             <b-tabs card>
                 <b-tab title="Candidates" class="col-md-6 tab-center" active>
-                        <p>Les candidats qui partagent le plus mes convictions
-                            <a href="#" class="badge badge-primary">+ d'infos</a>
-                        </p>
+                    <p>Les candidats qui partagent le plus mes convictions
+                        <a href="#" class="badge badge-primary">+ d'infos</a>
+                    </p>
 
-                        <candidate-lists class="list-scroll"></candidate-lists>
-                        
+                    <candidate-lists class="list-scroll"></candidate-lists>
+
                 </b-tab>
                 <b-tab title="Listes" class="col-md-6 tab-center">
-                    <ul class="list-group tab-scroll">
+                    <ul class="list-group tab-scroll" v-for="(score, idx) in electoral_list__scores" :key="idx">
                         <li class="list-group-item">
-                            <h4>Liste du Bourgmestre</h4>
+                            <h4>{{ electoral_lists.find(e => e.key == score.user_key).name }}</h4>
                             <div class="progress">
                                 <div class="progress-bar" role="progressbar" style="width: 75%;" aria-valuenow="75"
                                      aria-valuemin="0" aria-valuemax="100">75%
@@ -24,6 +24,17 @@
                 </b-tab>
             </b-tabs>
         </b-card>
+        
+        <!--
+
+        <p>{{ politicians.find(p => p.Key = politician_scores[0].user_key).first_name + " " + politicians.find(p => p.Key =
+            politician_scores[0].user_key).last_name }}</p>
+        
+        <p>{{ electoral_lists[0].name }} ({{ electoral_lists[0].candidates.find(c => c.key == politician_scores[0].user_key).order }})</p>
+        
+        <p>{{ politician_scores[0].score }}</p>
+        
+        -->
 
         <div>
             <button type="button" class="btn btn-default">Corriger mes réponses</button>
@@ -35,11 +46,46 @@
 <script>
 
     import CandidateLists from '@/components/CandidateList';
-    
+
     export default {
         name: 'results',
         components: {
             CandidateLists
+        },
+        data: () => {
+            return {
+                electoral_lists: [
+                    {
+                        key: "be_1435_cohesion",
+                        name: "CoHésion",
+                        candidates: [
+                            {
+                                "order": 1,
+                                "key": "be_politician_2"
+                            },
+                        ]
+                    }
+                ],
+                politicians: [
+                    {
+                        key: "be_politician_2",
+                        first_name: "John",
+                        last_name: "Doe"
+                    }
+                ],
+                politician_scores: [
+                    {
+                        user_key: "be_politician_2",
+                        score: 75.23
+                    }
+                ],
+                electoral_list__scores: [
+                    {
+                        user_key: "be_1435_cohesion",
+                        score: 50.3
+                    }
+                ]
+            }
         }
     }
 </script>
@@ -48,7 +94,7 @@
     .tab-center {
         margin: auto;
     }
-    
+
     .list-scroll {
         height: 70vh;
         overflow-y: scroll;
