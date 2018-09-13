97b28340baedcbbdfda5362375acdf41e2d9646f
Olivier de Marneffe <olivier@goshiplabs.com>
2018-09-13 11:25:11 +0200

    eslint

--- src/store/vote.js
+++ src/store/vote.js
@@ -1,78 +1,66 @@
-import API from "./_helpers/api";
-import Vue from "vue";
+import API from './_helpers/api'
+import Vue from 'vue'
 
 export default {
-    state: {
-        current: {
-            district: null,
-            election: null
-        },
-        districts: [],
-        districtSearchResults: [],
+  state: {
+    current: {
+      district: null,
+      election: null
     },
-    mutations: {
-        setCurrentDistrict(state, payload) {
-            state.current.district = payload;
-        },
-        setCurrentElection(state, payload) {
-            state.current.election = payload;
-        },
-        setDistricts(state, payload) {
-            state.districts = payload;
-        },
-        setDistrictSearchResults(state, payload) {
-            state.districtSearchResults = payload;
-        }
+    districts: [],
+    districtSearchResults: []
+  },
+  mutations: {
+    setCurrentDistrict (state, payload) {
+      state.current.district = payload
     },
-    actions: {
-        async getDistricts({ commit }, data) {
-            const districts = await API.get('/api/vote/district.json', data).then((request) => {
-
-                Vue.i18n.add('en', {vote: request.data.i18n.en})
-                Vue.i18n.add('fr', {vote: request.data.i18n.fr})
-                Vue.i18n.add('nl', {vote: request.data.i18n.nl})
-
-                return request.data.data
-            });
-
-            commit('setDistricts',districts);
-        },
-        filterDistricts({commit, state}, queryString){
-
-            const districts = state.districts.map(m => {
-                m.value = m.code + " " + Vue.i18n.translate('vote.' + m.name);
-                return m;
-            });
-            
-            const createDistrictFilter = function(queryString) {
-                return (district) => {
-                    return (district.value.toLowerCase().includes(queryString.toLowerCase()))
-                }
-            };
-            
-            const results = queryString ? districts.filter(createDistrictFilter(queryString)) : districts;
-            commit('setDistrictSearchResults',results);
-        },
-        
-        async setCurrentElection({commit}, district) {
-            const election = await API.get('/api/vote/election/2018_be_municipal/district/' + district.key + '.json').then((request) => {
-
-                Vue.i18n.add('en', {vote: request.data.i18n.en});
-                Vue.i18n.add('fr', {vote: request.data.i18n.fr});
-                Vue.i18n.add('nl', {vote: request.data.i18n.nl});
-
-                return request.data
-            });
-
-            console.log(election);
-            
-            commit('setCurrentElection',election);
-        }
+    setCurrentElection (state, payload) {
+      state.current.election = payload
     },
-    getters: {
-        districts: state => state.districts,
-        districtSearchResults: state => state.districtSearchResults,
-        currentElection: state => state.current.election
+    setDistricts (state, payload) {
+      state.districts = payload
+    },
+    setDistrictSearchResults (state, payload) {
+      state.districtSearchResults = payload
     }
-  
-};
+  },
+  actions: {
+    async getDistricts ({ commit }, data) {
+      const districts = await API.get('/api/vote/district.json', data)
+        .then((request) => {
+          Vue.i18n.add('en', {vote: request.data.i18n.en})
+          Vue.i18n.add('fr', {vote: request.data.i18n.fr})
+          Vue.i18n.add('nl', {vote: request.data.i18n.nl})
+          return request.data.data
+        })
+      commit('setDistricts', districts)
+    },
+    filterDistricts ({commit, state}, queryString) {
+      const districts = state.districts.map(m => {
+        m.value = m.code + ' ' + Vue.i18n.translate('vote.' + m.name)
+        return m
+      })
+      const createDistrictFilter = function (queryString) {
+        return (district) => {
+          return (district.value.toLowerCase().includes(queryString.toLowerCase()))
+        }
+      }
+      const results = queryString ? districts.filter(createDistrictFilter(queryString)) : districts
+      commit('setDistrictSearchResults', results)
+    },
+    async setCurrentElection ({commit}, district) {
+      const election = await API.get('/api/vote/election/2018_be_municipal/district/' + district.key + '.json').then((request) => {
+        Vue.i18n.add('en', {vote: request.data.i18n.en})
+        Vue.i18n.add('fr', {vote: request.data.i18n.fr})
+        Vue.i18n.add('nl', {vote: request.data.i18n.nl})
+        return request.data
+      })
+      commit('setCurrentElection', election)
+    }
+  },
+  getters: {
+    districts: state => state.districts,
+    districtSearchResults: state => state.districtSearchResults,
+    currentElection: state => state.current.election
+  }
+}
