f0d0f87b569e2a01bf566385ddb000d27d4ccda9
Daniel Sum <daniel@lacerisenumerique.be>
2018-09-18 16:02:57 +0200

    cleaning api

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,17 +2,18 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 
-router.get('/dir/politician/:key', function (req, res, next) {
+router.get('/dir/politician/:key.json', function (req, res, next) {
 
   var key = req.params['key'];
 
   key = key.replace('politician_be_', '');
 
-  var test = db.query("SELECT * from politician WHERE id = ?", key, function (err, rows, fields) {
+  db.query("SELECT * from politician WHERE id = ?", key, function (err, rows, fields) {
     if (err) throw err;
 
     let item = rows[0];
 
+    // Generate thumb
     item.thumb = "http://directory.wecitizens.be/assets/media/politician-thumb/"+item.id+".jpg";
 
     res.json(item);
