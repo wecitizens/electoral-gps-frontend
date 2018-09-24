15dbf738edb3edd7ce45a1e4f9ba1ebafaca9858
Daniel Sum <daniel@lacerisenumerique.be>
2018-09-24 10:57:00 +0200

    - add deploy

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,7 @@
 
 rsync -azP dist ubuntu@ec2-52-47-66-164.eu-west-3.compute.amazonaws.com:/home/user/web/gps/
 
+rsync -azP api ubuntu@ec2-52-47-66-164.eu-west-3.compute.amazonaws.com:/home/user/web/
+
 # Force to put correct access right
 ssh -i ".ssh/wecitizens.pem" ubuntu@ec2-52-47-66-164.eu-west-3.compute.amazonaws.com 'sudo chmod -R 777 /home/user/web/gps/dist/'
